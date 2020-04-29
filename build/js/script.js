88d55b21205111ab8d38c117c803a607c4196bb1
Ksenia Balashova <utyatka@gmail.com>
2020-04-29 23:27:06 +0200

    fix

--- build/js/script.js
+++ build/js/script.js
@@ -28978,6 +28978,15 @@ var widgetsTooltip = $.ui.tooltip;
 
   }));
 
+$(document).ready(function () {
+  $("#choose").on("click", function (event) {
+    event.preventDefault();
+    var id = $(this).attr("href"),
+      top = $(id).offset().top + $(id).height() / 2 - $(window).height() / 2;
+    setTimeout(() => $("body,html").animate({ scrollTop: top }, 1000), 500);
+  });
+});
+
 (() => {
   const MOBILE = 767;
   const TABLET = 1023;
