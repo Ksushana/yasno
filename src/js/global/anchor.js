1d8d38f1bdf2d654d88409d8c6f0c5fa25edf45a
Ksenia Balashova <utyatka@gmail.com>
2020-04-29 23:38:25 +0200

    scroll smooth

--- src/js/global/anchor.js
+++ src/js/global/anchor.js
@@ -1,8 +1,1 @@
-$(document).ready(function () {
-  $("#choose").on("click", function (event) {
-    event.preventDefault();
-    var id = $(this).attr("href"),
-      top = $(id).offset().top + $(id).height() / 2 - $(window).height() / 2;
-    setTimeout(() => $("body,html").animate({ scrollTop: top }, 1000), 500);
-  });
-});
+var scroll = new SmoothScroll('a[href*="#"]');
